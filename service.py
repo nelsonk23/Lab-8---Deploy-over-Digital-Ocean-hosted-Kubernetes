a6fe16aa8a0a9183a92aad4620266b31439ad3c1
nelsonk23 <nelsonk@msoe.edu>
2025-03-30 18:56:47 -0500

    Update service.py

--- service.py
+++ service.py
@@ -1,1 +1,26 @@
+from models import ToDoModel
+
+class ToDoService:
+    def __init__(self):
+        self.model = ToDoModel()
+
+    def create(self, params):
+        """Creates a new To-Do item"""
+        return self.model.create(params)
+
+    def update(self, item_id, params):
+        """Updates an existing To-Do item"""
+        return self.model.update(item_id, params)
+
+    def delete(self, item_id):
+        """Soft deletes a To-Do item"""
+        return self.model.delete(item_id)
+
+    def list(self):
+        """Lists all non-deleted To-Do items"""
+        return self.model.list_items()
+
+    def get_by_id(self, item_id):
+        """Retrieves a specific To-Do item by its ID"""
+        return self.model.get_by_id(item_id)
 
